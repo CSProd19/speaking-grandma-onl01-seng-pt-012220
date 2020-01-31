5fa126a199ac71eb292831674851e73252815ed4
Maria Connie Sanchez <msnchz04@gmail.com>
2020-01-31 00:53:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,13 +14,12 @@
 
 def speak_to_grandma(phrase)
   
-  phrase == "I LOVE YOU GRANDMA!"
   
-    if "phrase.downcase"
-      puts "HUH?! SPEAK UP, SONNY!"
-  elsif "phrase.upcase"
-      puts "NO, NOT SINCE 1938!"
-  elsif "phrase"
+    if phrase == "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+      puts "NO, NOT SINCE 1938!"
+  else 
+      puts "HUH?! SPEAK UP, SONNY!"
 end
 end
